57fd69abc1372926c2fc9c4fba806e0adb596c82
Marc-André Arseneault <marc-andre@neaf.co>
2021-05-12 10:36:30 -0400

    chore: fix the serverless app file

--- src/app.js
+++ src/app.js
@@ -1,17 +1,30 @@
-/* eslint-disable node/no-process-env */
 /* eslint-disable node/no-process-exit */
-const fastify = require("fastify")({
+/* eslint-disable node/no-process-env */
+/* eslint-disable import/no-default-export */
+const dotenv = require("dotenv");
+const Fastify = require("fastify");
+
+dotenv.config();
+
+// Instantiate Fastify with some config
+const app = Fastify({
   logger: true,
 });
 
-fastify.register(require("./badges/peppy"));
+app.register(require("./badges/peppy"));
 
-const start = async () => {
-  try {
-    await fastify.listen(process.env.PORT || 3000);
-  } catch (error) {
-    fastify.log.error(error);
-    process.exit(1);
-  }
+if (process.env.DEV) {
+  const start = async () => {
+    try {
+      await app.listen(process.env.PORT || 3000);
+    } catch (error) {
+      app.log.error(error);
+      process.exit(1);
+    }
+  };
+  start();
+}
+module.exports = async (req, res) => {
+  await app.ready();
+  app.server.emit("request", req, res);
 };
-start();
